3665d082643cb60767d0dc111c60bd67d87e5ffb
Schmidt-Larsen <80283425+Schmidt-Larsen@users.noreply.github.com>
2022-12-17 21:07:39 +0100

    test

--- Chapter 2 solutions.R
+++ Chapter 2 solutions.R
@@ -103,3 +103,4 @@ z[1] <- 0 #Setting first element of z to 0
 Id <- diag(10)> Id <- 5*Id # one way, using the fact that Id is the identity
 Id[Id != 0] <- 5 # another way, using vector indexing of a matrix7
 
+# end
